7171044e9054985f95b14176819273212dcde5ff
deartanapol10@gmail.com <deartanapol10@gmail.com>
2019-04-29 05:20:28 +0700

    Add onchange and connect state to value

--- src/App.js
+++ src/App.js
@@ -15,45 +15,71 @@ class App extends Component {
          reducer,
          {} /* Initial State (Optional)*/,
          compose(
-            applyMiddleware(ReduxThunk),
+            applyMiddleware(ReduxThunk)
             //window.__REDUX_DEVTOOLS_EXTENSION__ &&
-             //  window.__REDUX_DEVTOOLS_EXTENSION__()
+            //  window.__REDUX_DEVTOOLS_EXTENSION__()
          )
       );
       return (
          <Provider store={store}>
             <React.Fragment>
-            <header className="site-header">
-               <div className="container">
-                  <div className="site-header__wrapper">
-                     <h1 className="site-logo"><a className="site-logo__link" href="#">Nutricia Expert</a></h1>
-                     <nav className="site-nav">
-                        <ul className="menu">
-                           <li className="menu-item"><a href="#">ประเมินสุขภาพลูกน้อย</a></li>
-                           <li className="menu-item"><a href="#">คำถามที่พบบ่อย</a></li>
-                           <li className="menu-item"><a href="#">ฝากคำถามถึงคุณหมอ</a></li>
-                           <li className="menu-item"><a href="#">สาระน่ารู้จากคุณหมอ</a></li>
-                        </ul>
-                     </nav>
+               <header className="site-header">
+                  <div className="container">
+                     <div className="site-header__wrapper">
+                        <h1 className="site-logo">
+                           <a className="site-logo__link" href="#">
+                              Nutricia Expert
+                           </a>
+                        </h1>
+                        <nav className="site-nav">
+                           <ul className="menu">
+                              <li className="menu-item">
+                                 <a href="#">ประเมินสุขภาพลูกน้อย</a>
+                              </li>
+                              <li className="menu-item">
+                                 <a href="#">คำถามที่พบบ่อย</a>
+                              </li>
+                              <li className="menu-item">
+                                 <a href="#">ฝากคำถามถึงคุณหมอ</a>
+                              </li>
+                              <li className="menu-item">
+                                 <a href="#">สาระน่ารู้จากคุณหมอ</a>
+                              </li>
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </header>
+               <div className="site-content">
+                  <div className="app">
+                     <Main />
                   </div>
                </div>
-            </header>
-            <div className="site-content">
-               <div className="app">
-                  <Main />
-               </div>
-            </div>
-            <footer className="site-footer">
-               <div className="container">
-                  <div className="site-footer__wrapper">
-                     <div className="call24"><a className="call24__link" href="tel:+6627403456"><span>Careline</span> 0-2740-3456</a></div>
-                     <div className="contact-specialist"><a href="https://nutriciaexpert.com/careline" target="_blank">ติดต่อผู้เชี่ยวชาญ</a></div>
-                     <div className="copyrights">&copy; 2019 Dumex (Thailand) Ltd. All Rights Reserved.</div>
+               <footer className="site-footer">
+                  <div className="container">
+                     <div className="site-footer__wrapper">
+                        <div className="call24">
+                           <a className="call24__link" href="tel:+6627403456">
+                              <span>Careline</span> 0-2740-3456
+                           </a>
+                        </div>
+                        <div className="contact-specialist">
+                           <a
+                              href="https://nutriciaexpert.com/careline"
+                              target="_blank"
+                           >
+                              ติดต่อผู้เชี่ยวชาญ
+                           </a>
+                        </div>
+                        <div className="copyrights">
+                           &copy; 2019 Dumex (Thailand) Ltd. All Rights
+                           Reserved.
+                        </div>
+                     </div>
                   </div>
-               </div>
-            </footer>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-            <script src="./js/main.min.js"></script>
+               </footer>
+               <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" />
+               <script src="./js/main.min.js" />
             </React.Fragment>
          </Provider>
       );
--- src/components/pages/YouOrWho.js
+++ src/components/pages/YouOrWho.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { changeStepAction, youOrWhoAction } from "../../actions";
 
-import { Button } from "../reuse";
+// import { Button } from "../reuse";
 
 class YouOrWho extends Component {
    changeStep = (newStep, newMember) => {
@@ -28,14 +28,39 @@ class YouOrWho extends Component {
             <Button onClick={this.changeStep.bind(this, "3", false)}>
                เริ่มต้นทำแบบประเมิน
             </Button> */}
-            <h1 className="header">ทดสอบความเสี่ยง <strong>ภูมิแพ้</strong> ของลูกน้อย<br />
-            รู้เร็ว รู้ง่าย เพียง 1 นาที</h1>
-            <h2 className="sub-header">สวัสดีคุณ {this.props.Member.firstname || ""}</h2>
+            <h1 className="header">
+               ทดสอบความเสี่ยง <strong>ภูมิแพ้</strong> ของลูกน้อย
+               <br />
+               รู้เร็ว รู้ง่าย เพียง 1 นาที
+            </h1>
+            <h2 className="sub-header">
+               สวัสดีคุณ {this.props.Member.firstname || ""}
+            </h2>
             <div className="page-content you-or-who-content">
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-               <p><a href="#" onClick={this.changeStep.bind(this, "4", true)}>คุณไม่ใช่บุคคลที่กล่าวถึง?</a></p>				
+               <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+               </p>
+               <p>
+                  <a href="#" onClick={this.changeStep.bind(this, "4", true)}>
+                     คุณไม่ใช่บุคคลที่กล่าวถึง?
+                  </a>
+               </p>
             </div>
-            <p><button className="button button_solid" onClick={this.changeStep.bind(this, "3", false)}>เริ่มต้นทำแบบประเมิน</button></p>
+            <p>
+               <button
+                  className="button button_solid"
+                  onClick={this.changeStep.bind(this, "3", false)}
+               >
+                  เริ่มต้นทำแบบประเมิน
+               </button>
+            </p>
          </React.Fragment>
       );
    }
